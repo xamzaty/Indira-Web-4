5b367f2bc01e2e90a22a56483ca91756d7bbfc65
xamzaty <h.yerzhanov@gmail.com>
2020-10-16 16:19:04 +0600

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+<script>
+var swiper = new Swiper('.swiper-container', {
+    direction: 'vertical',
+    slidesPerView: 1,
+    spaceBetween: 30,
+    mousewheel: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+});
+</script>
